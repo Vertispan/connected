d1df65dd40020b8875025b5c352adcb003a23cd4
Colin Alworth <colin@redcurrent.com>
2017-10-11 14:05:17 -0500

    differently monkeypatch jsinterop-base

--- src/main/java/com/vertispan/draw/connected/client/FlowChartEntryPoint.native.js
+++ src/main/java/com/vertispan/draw/connected/client/FlowChartEntryPoint.native.js
@@ -1,8 +1,8 @@
-goog.require('jsinterop.base.InternalJsUtil');
+var internalJsUtil = goog.require('jsinterop.base.InternalJsUtil');
 
 // monkeypatch the bits of jsinterop-base we use
-goog.module.get('jsinterop.base.InternalJsUtil').getIndexed=function(array, index) { return array[index]; };
-goog.module.get('jsinterop.base.InternalJsUtil').getLength=function(array) { return array.length; };
+internalJsUtil.getIndexed=function(array, index) { return array[index]; };
+internalJsUtil.getLength=function(array) { return array.length; };
 
 // Seems this can't run right away, circular deps haven't been resolved yet.
 setTimeout(function(){ FlowChartEntryPoint.$create__().m_onModuleLoad__()}, 0);
