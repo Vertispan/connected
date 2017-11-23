29fe6c75ad272805942c165b9b6233d6ee643602
Colin Alworth <colin@colinalworth.com>
2017-11-23 12:03:10 -0600

    squish entrypoint wiring magic

--- src/main/java/com/vertispan/draw/connected/client/FlowChartEntryPoint.native.js
+++ src/main/java/com/vertispan/draw/connected/client/FlowChartEntryPoint.native.js
@@ -1,9 +1,38 @@
+/*
+ * #%L
+ * Connected
+ * %%
+ * Copyright (C) 2017 Vertispan
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 var internalJsUtil = goog.require('jsinterop.base.InternalJsUtil');
 
-// monkeypatch the bits of jsinterop-base we use
+// TODO build jsinterop-base correctly
+// Monkeypatch the bits of jsinterop-base we use
 internalJsUtil.getIndexed=function(array, index) { return array[index]; };
 internalJsUtil.getLength=function(array) { return array.length; };
 
-// Seems this can't run right away, circular deps haven't been resolved yet.
-setTimeout(function(){ FlowChartEntryPoint.$create__().m_onModuleLoad__()}, 0);
+// Defer this command, since this will be folded into the FlowChartEntryPoint js impl,
+// and if it runs right away, will not have its dependencies resolved yet (at least while
+// running in BUNDLE).
+setTimeout(function(){
+  // Call the java "constructor" method, `new` will only work if it is a @JsType, or maybe
+  // once optimized. Without this, in BUNDLE mode, `new` doesn't include the clinit, so
+  // static imports haven't been resolved yet.
+  var ep = FlowChartEntryPoint.$create__();
+  // Invoke onModuleLoad to start the app.
+  ep.m_onModuleLoad__()
+}, 0);
 
